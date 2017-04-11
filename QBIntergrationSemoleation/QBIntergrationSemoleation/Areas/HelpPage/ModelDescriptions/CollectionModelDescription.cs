94172967868aea96820736749e050a183725bb10
mh884 <mh884@live.ca>
2017-04-10 21:32:24 -0400

    implement AntiForgeryToken

---
+++ QBIntergrationSemoleation/QBIntergrationSemoleation/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace QBIntergrationSemoleation.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ QBIntergrationSemoleation/QBIntergrationSemoleation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QBIntergrationSemoleation.WebApiApplication" Language="C#" %>
